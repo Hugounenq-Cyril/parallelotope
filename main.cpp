18d79396ac29fa5b85a3d055ea75f16fe043be75
Francisco <fvialp@gmail.com>
2018-10-09 10:31:11 -0300

    bfs algorithm, not giving correct results

--- main.cpp
+++ main.cpp
@@ -6,38 +6,119 @@ int secret_vector[N] = {6,0,1,0,1,0,0};
 int secret_vector_inv[N] = {
         270125,17351,-50419,10547,-35236,-8290,21842,-45397};
 
-int _DEN = 1624707;
+int DET= 1624707;
+int diameter; // = sum (secret[i])
 int ** B    = rotation_matrix(secret_vector); 
 int ** Binv = rotation_matrix(secret_vector_inv); 
-    
+
+int calls = 0;
 
 int main(int argc, char** argv){
 
+    cout << "Secret vector, inverse, and determinant" << endl;
     print_point(secret_vector);
     print_point(secret_vector_inv);
+    cout << DET << endl;
 
     print_matrix(B);  
     print_matrix(Binv);  
-    
-    int p[N] = {0,0,0,0,0,0,0,0};
-    cout << is_in(p) << endl;
 
-    while(is_in(p)){
-            int i = 7;
-            p[i]++;
 
+    // First find vertex along <(1,1,...,1)>
+    diameter = 0;
+    for(int i = 0; i < N; i++){
+        diameter += secret_vector[i];
     }
-    print_point(p);
+
+    int starting_point[N];
+    for(int i = 0; i < N; i++){
+        starting_point[i] = -diameter/2 + 1;
+    }
+    
+    int * point = starting_point;
+
+   
+    move(point);
+   
+
     return 0;
 }
 
 
+int * move(int* p){
+
+    int * next_point = new int [N];
+
+    for(int i = 0; i < N; i++){
+        next_point[i] = p[i];
+    }
+
+    while(is_in(next_point)){
+        next_point[0]++;
+    }
+    next_point[0]--;
+
+
+    for(int j = 1; j < N; j ++)
+    {
+        next_point[j] ++ ;
+        if(is_in(next_point)) move(next_point);
+        else{next_point[j] -- ;}
+    }
+    
+    char c;
+    cin >> c ;
+    
+    int * candidate_vertex = new int[N];
+    for(int i = 0; i < N; i++){
+        candidate_vertex[i] = p[i] + diameter/2;
+    }
+
+    // We are maybe on a vertex, or on a side.
+    cout << "Calls so far :" << calls << endl;
+    cout << "Vertex? :";
+    print_point(candidate_vertex);
+
+    
+    return p;
+
+}
+
+int * old_move(int* p){
+    // Add all coordinates but the first.
+    int * next_point = new int [N];
+    next_point[0] = p[0];
+    for(int i = 1; i < N; i++){
+        next_point[i] = p[i] + 1;
+    }
+    
+    // Return closest point to <e_1,...,e_[N-1]>
+    for(int j = 1; j < N; j++)
+    {
+        if(is_in(next_point)) return next_point;
+        next_point[j] -- ;
+    }
+    
+    // We are maybe on a vertex
+    cout << "vertex? : ";
+    print_point(next_point);
+
+    // No more coordinates can be added, now add the first
+    next_point[0] ++;
+
+    if(!is_in(next_point)) return p;
+    return next_point;
+
+}
+
 bool is_in(int* p){
     
-    double * res = matrix_vector_den(Binv, p, _DEN);
+    calls ++;
+
+    double * res = matrix_vector_den(Binv, p, DET);
     
     for(int i = 0; i < N; i++){
-        if((res[i] < -0.5)||(res[i]>=0.5)) return false;
+        if((res[i] < -0.5)||(res[i]>0.5)) return false;
     }
     return true;
 }
@@ -64,8 +145,8 @@ int** rotation_matrix(int* v){
     return res;
 }
 
-
-double* matrix_vector_den(int ** A, int* v, int den){
+// Computes (1/det)* A * v  
+double* matrix_vector_den(int ** A, int* v, int det){
 
     double * res = new double[N];
 
@@ -74,11 +155,11 @@ double* matrix_vector_den(int ** A, int* v, int den){
         for(int j = 0; j < N; j++){
             res[i] += A[i][j] * v[j];
         }
-        res[i] /= den;
+        res[i] /= det;
     }
-    return res;
-    
+    return res;    
 }
+
 
 void print_point(int* p){
     for(int i = 0; i < N-1; i++){
@@ -107,7 +188,7 @@ void print_matrix(int** p){
         }  
         cout << p[k][N-1] << endl;
     }
-    cout << "**** END MATRIX ****" << endl;
+    cout << "****  END MATRIX  ****" << endl;
 
 }
 
--- main.hpp
+++ main.hpp
@@ -10,43 +10,16 @@
 
 int** rotation_matrix(int*);
 
-// Computes (1/den)* A * v  
+// Computes (1/det)* A * v  
 double* matrix_vector_den(int**, int*, int);
-void print_matrix(int** p);
 
+int * move (int *);
 
 
 bool is_in(int* p);
 
-double* half(double* , double* );
-
-//bool diff(int*, int*);
-bool diff(double*, double*);
-		
-double* binary_move(double* , double*);
-
-double * fall(double *,double *);
-double * fallnew(double *,double *);
-
-
-double* normalized(int* p);
-double* normalized(double* p);
-
-double * next_dir(double *);
-double * next_point(double *);
-double * next_point_static(double *);
-
+void print_matrix(int** p);
 void print_point(int* p);
 void print_point(double* p);
 
-double** gram_schmidt(double* dir);
-double** gram_schmidt_orth(double* dir);
-
-double inner_prod(double* u, double* v);
-
-bool is_orthonormal(double **m);
-
-int test_neighbour(double * point, double * dir);
-
-double norm(double* u);
 
